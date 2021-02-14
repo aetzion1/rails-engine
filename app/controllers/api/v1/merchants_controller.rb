926e846923af75750a684164a8c25a6abb8e6845
Adam Etzion <adametzion90@gmail.com>
2021-02-13 17:03:52 -0700

    Add pagination code to merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,25 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Merchant.all)
+    merchants = Merchant.all
+    merchants = Merchant.page(page).per_page(per_page)
+    render json: MerchantSerializer.new(merchants)
+  end
+
+  private
+
+  def page
+    @page ||= params[:page] || 1
+  end
+
+  def per_page
+    @per_page ||= params[:per_page] || 20
   end
 end
+
+# respond_with. merchants, meta: {
+#   current_page: merchants.current_page,
+#   next_page: merchants.next_page,
+#   prev_page: merchants.prev_page,
+#   total_pages: merchants.total_pages,
+#   total_count: merchants.total_count
+# }
