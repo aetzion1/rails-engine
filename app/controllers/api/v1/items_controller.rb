56d38110af407db86e2549d4cc9b64167f47edb3
Adam Etzion <adametzion90@gmail.com>
2021-02-16 14:18:28 -0700

    Add item moel, controller, serializer, and realtionships to merchants

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    render json: ItemSerializer.new(@items)
+
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    ItemSerializer.new(@item)
+
+    # options[:include] = [:stores, :'stores.name']
+    # serializable_hash = BookSerializer.new([@book], options).serializable_hash
+    # require 'pry'; binding.pry
+
+  end
+
+  # def create
+  #   render json: Book.create(book_params)
+  # end
+
+  # def update
+  #   render json: Book.update(params[:id], book_params)
+  # end
+
+  # def destroy
+  #   render json: Book.delete(params[:id])
+  # end
+
+  private
+    def item_params
+      params.require(:item).permit(
+                                    :name,
+                                    :description,
+                                    :unit_price
+                                  )
+    end
+end
