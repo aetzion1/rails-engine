fb932828844a8f7a8d1c70b390d8b9e623668eb9
Adam Etzion <adametzion90@gmail.com>
2021-02-16 14:12:24 -0700

    Add route and controller logic to get one merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    merchants = Merchant.limit(per_page).offset(page)
-    render json: MerchantSerializer.new(merchants)
+    @merchants = Merchant.limit(per_page).offset(page)
+    render json: MerchantSerializer.new(@merchants)
+  end
+
+  def show
+    @merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(@merchant)
   end
 
   private
