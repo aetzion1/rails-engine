821057bd32f2c22e2206fdeee48f7baa5f085d69
Adam Etzion <adametzion90@gmail.com>
2021-02-17 20:43:23 -0700

    Add rubocop formatting fixes

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,25 +1,22 @@
 class Api::V1::Items::SearchController < ApplicationController
-
-def index
-  @items = if item_params[:name].blank?
-    nil
-  else
-    Item.where('LOWER(name) LIKE ?', "%#{item_params[:name].downcase}%")
+  def index
+    @items = if item_params[:name].blank?
+               nil
+             else
+               Item.where('LOWER(name) LIKE ?', "%#{item_params[:name].downcase}%")
+             end
+    # ternary operator
+    render json: (@items ? ItemSerializer.new(@items) : { data: [] })
   end
-# ternary operator
-  render json: (@items ? ItemSerializer.new(@items) : { data: [] })
-end
 
-private
+  private
 
-def item_params
-  params.permit(
-    :name,
-    :description,
-    :unit_price,
-    :merchant_id
-  )
-end
-
-
+  def item_params
+    params.permit(
+      :name,
+      :description,
+      :unit_price,
+      :merchant_id
+    )
+  end
 end
