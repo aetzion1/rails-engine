4409b5837a502c29d1610bae9306c9f7c1e34680
Adam Etzion <adametzion90@gmail.com>
2021-02-18 19:28:07 -0700

    Add model spec udpates

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Merchant, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+  end
+
+  describe "relationships" do
+    it { should have_many :invoices }
+    it { should have_many :items }
+  end
+
+  describe "class methods" do
+  end
+end
