936f5cfa6f8832f2955aa98ab9117944f84d5847
Adam Etzion <adametzion90@gmail.com>
2021-02-19 11:41:01 -0700

    Update revenue by date method, replace where string with symbols

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def find_one
     # return nil if merchant_params[:name].blank?
-    @merchant = if merchant_params[:name].blank?
+    merchant = if merchant_params[:name].blank?
                   nil
                 else
                   Merchant.where('LOWER(name) LIKE ?', "%#{merchant_params[:name].downcase}%").first
+                  # Try this: negates need for LOWER or downcase. Also helps manage weird characters.
+                  # Merchant.where("#{merchant_params[:name]} ilike '%#{merchant_params[:nameto_sym]}'").first
                 end
-    # ternary operator
-    render json: (@merchant ? MerchantSerializer.new(@merchant) : { data: {} })
+    response = MerchantSerializer.new(merchant)
+    render json: (merchant ? response : { data: {} })
   end
 
   private
