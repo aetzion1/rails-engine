fb42131d8efa0cf56393a7e7a7a6d9b43780d693
Adam Etzion <adametzion90@gmail.com>
2021-02-19 10:34:52 -0700

    Update revenue namespace routing, move revenue methods accross controllers into single revenue_controller

--- app/controllers/api/v1/revenue/revenue_controller.rb
+++ app/controllers/api/v1/revenue/revenue_controller.rb
@@ -1,12 +1,26 @@
-class Api::V1::Revenue::MerchantsController < ApplicationController
-  def index
+class Api::V1::Revenue::RevenueController < ApplicationController
+
+  def between_dates
+    unless params[:start].present? && params[:end].present?
+      return render json: { error: 'Specify dates' },
+                    status: '400'
+    end
+
+    # params[:start].present?
+    # return render json: { error: 'Specify dates' }, status: '400' unless params[:start_date].to_datetime < params[:start_date].to_datetime
+
+    @revenue = Merchant.revenue_by_date(params[:start], params[:end])
+    render json: RevenueSerializer.get_revenue(@revenue)
+  end
+  
+  def merchants
     return render json: { error: 'Specify a quantity' }, status: '400' unless params[:quantity]
 
     @merchants = Merchant.most_revenue(revenue_params[:quantity])
     render json: MerchantNameRevenueSerializer.new(@merchants)
   end
 
-  def show
+  def merchant
     unless Merchant.find(merchant_params[:id].to_i)
       return render json: { error: 'Merchant doesnt exist' },
                     status: '400'
@@ -26,4 +40,5 @@ class Api::V1::Revenue::MerchantsController < ApplicationController
   def merchant_params
     params.permit(:id)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-# MERCHANTS routes
 namespace :api do
   namespace :v1 do
+
     namespace :revenue do
-      get '/merchants', to: 'merchants#index'
-      resources :merchants, only: [:show]
+      get '/', to: 'revenue#between_dates', as: :revenue_between_dates
+      get '/merchants', to: 'revenue#merchants', as: :merchants_revenue
+      get '/merchants/:id', to: 'revenue#merchant', as: :merchant_revenue
     end
+
     namespace :merchants do
       get '/find', to: 'search#show', as: :find_merchant
       get '/most_items', to: 'most_items#index'
@@ -19,7 +21,7 @@ namespace :api do
     resources :items, only: [:index, :show, :create, :new, :update, :destroy] do
       get '/merchant', to: 'items/merchants#show'
     end
-    resources :revenue, only: [:index]
+    # resources :revenue, only: [:index]
   end
 end
 
